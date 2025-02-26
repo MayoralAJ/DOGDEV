4fe9029e0c074e436f973b10b5d46a01d0ef699f
MayoralAJ <ana.mayoral.1bmangal@gmail.com>
2025-02-26 16:47:12 +0000

    .

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,4 +15,10 @@ menuOpener.addEventListener('click', ()=>{
         menuOpener.querySelector('.hamburguer-icon')
         .style .display='none';
     }
-})
+});
+let testemonials = [
+    {quote:' "Mais do que nunca, os animais '+'São tratados como membros da família"' ,origin:'cbs.svg'}, 
+    {quote:'"DogDev é um serviço de entrega direto ao '+'consumidor, preparado na hora com ingredientes 100% '+'reais. Ingredientes que humanos reconheceriam"' ,origin:'forbes.svg'},  
+    {quote:'"DogDev usa ingredientes simples e limpos em '+'seus produtos."' ,origin:'fox.svg'}, 
+    {quote:'Vejo você DogDev como um verdadeiro herói' ,origin:'sharktank.svg'}
+];
