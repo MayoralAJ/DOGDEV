a1e21bfab922b02c7fed40a5cc18edb35055cd9b
MayoralAJ <ana.mayoral.1bmangal@gmail.com>
2025-02-26 18:24:57 +0000

    .

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,3 +22,36 @@ let testemonials = [
     {quote:'"DogDev usa ingredientes simples e limpos em '+'seus produtos."' ,origin:'fox.svg'}, 
     {quote:'Vejo você DogDev como um verdadeiro herói' ,origin:'sharktank.svg'}
 ];
+//captura os elementos do html usando querySelector
+let  testemonialQuote = 
+document.querySelector('.testemonials .quote');
+let  testemonialIcons = 
+document.querySelector('.testemonials .icons');
+
+//repetição para criar as imagens
+for(let tindex in testemonials){
+    let img = document.createElement('img');
+    img.setAttribute('src', './assets/images/'+ testemonials[parseInt(tiindex)]);
+    img.style.cursor= 'pointer';
+    img.addEventListener('click', ()=>fillTestemonial(parseInt(tindex)));
+    testemonialIcons.appendChild(img);
+    let currentTestemonial = 0;
+    let testemonialTimer = 0;
+    const fillTestemonial = (index) =>{
+        clearTimeout(testemonialTimer);
+        currentTestemonial = index;
+        testemonialQuote.innerHTML = testemonials[currentTestemonial].quote;
+        let imgs = testemonialsIcons.querySelectorAll('img');
+        for(let img of imgs) img.style.opacity=0.2;
+        imgs[currentTestemonial].style.opacity=1;
+        testemonialTimer = setTimeout(nextTestemonials, 3000);
+    }
+    const nextRestemonial = () =>{
+        if(testemonials[currentTestemonial+1]){
+            fillTestemonial(currentTestemonial+1);
+        }else{
+            fillTestemonial(0);
+        }
+    }
+    nextTestemonial();
+}
